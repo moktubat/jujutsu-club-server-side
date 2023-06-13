ff7b7cd6981ec4a5bafb33d64302640b668142de
moktubat <shadhinreja@gmail.com>
2023-06-13 08:43:05 +0600

    selected class delete with id

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.odqhq4i.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,7 +26,9 @@ async function run() {
     await client.connect();
 
     const classesCollection = client.db("sumCampDB").collection("classes");
-    const instructorsCollection = client.db("sumCampDB").collection("instructors");
+    const instructorsCollection = client
+      .db("sumCampDB")
+      .collection("instructors");
     const selectedCollection = client.db("sumCampDB").collection("selected");
 
     app.get("/classes", async (req, res) => {
@@ -56,7 +58,9 @@ async function run() {
     app.get("/popularInstructor", async (req, res) => {
       try {
         const popularInstructor = await instructorsCollection.find().toArray();
-        const sortedData = popularInstructor.sort((a, b) => b.students - a.students);
+        const sortedData = popularInstructor.sort(
+          (a, b) => b.students - a.students
+        );
 
         const sixInstructor = sortedData.slice(0, 6);
 
@@ -68,21 +72,28 @@ async function run() {
     });
 
     // selected classes api
-    app.get('/selected', async(req, res) => {
+    app.get("/selected", async (req, res) => {
       const email = req.query.email;
-      if(!email){
+      if (!email) {
         res.send([]);
       }
-      const query = { email: email};
+      const query = { email: email };
       const result = await selectedCollection.find(query).toArray();
       res.send(result);
-    })
-    app.post('/selected', async(req, res) => {
+    });
+    app.post("/selected", async (req, res) => {
       const item = req.body;
       console.log(item);
       const result = await selectedCollection.insertOne(item);
       res.send(result);
-    })
+    });
+
+    app.delete("/selected/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectedCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
