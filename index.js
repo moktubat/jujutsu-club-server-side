0283eea0bb480d6624247f2d9922114ac64bc727
moktubat <shadhinreja@gmail.com>
2023-06-14 01:30:49 +0600

    users api created

--- index.js
+++ index.js
@@ -25,17 +25,35 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("sumCampDB").collection("users");
     const classesCollection = client.db("sumCampDB").collection("classes");
     const instructorsCollection = client
       .db("sumCampDB")
       .collection("instructors");
     const selectedCollection = client.db("sumCampDB").collection("selected");
 
+    // user related api
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    // classes related api
     app.get("/classes", async (req, res) => {
       const result = await classesCollection.find().toArray();
       res.send(result);
     });
-
+    // instructors related api
     app.get("/instructors", async (req, res) => {
       const result = await instructorsCollection.find().toArray();
       res.send(result);
@@ -92,7 +110,7 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await selectedCollection.deleteOne(query);
-      res.send(result);
+      res.send({ deleteCount: result.deletedCount });
     });
 
     // Send a ping to confirm a successful connection
